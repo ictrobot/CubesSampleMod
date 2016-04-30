31e4ca31e5fb7d793472cd6b5b06ca12fbd50b5b
ictrobot <ictrobot@outlook.com>
2016-04-30 12:17:27 +0100

    Update

--- src/example/MyMod.java
+++ src/example/MyMod.java
@@ -12,12 +12,10 @@ public class MyMod {
 
   private Block myBlock;
 
-  //Methods annotated with ModEventHandler can be called whatever you want
-  //The parameter tells Cubes the type of ModEvent the method handles
   @ModEventHandler
   public void preInit(PreInitializationEvent event) {
     Log.info("Hello World");                            //Log "Hello World"
-    myBlock = new Block("MyMod:block/texture");         //Make instance of a custom block
+    myBlock = new Block("MyMod:texture");               //Make instance of a custom block
     IDManager.register(myBlock);                        //Register MyBlock
   }
 }
